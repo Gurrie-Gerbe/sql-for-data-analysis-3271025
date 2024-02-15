199cf95cbad3ecf45705424284cbb8dffdf8fbdd
Gurrie-Gerbe <gerbe.van.dreumel@gmail.com>
2024-02-15 11:43:51 +0000

    Own Changes to create extra column with

--- Chapter 4/Common SQL Aggregate Functions/Create View.sql
+++ Chapter 4/Common SQL Aggregate Functions/Create View.sql
@@ -3,7 +3,8 @@ SELECT State,
   AVG(TotalDue) as Avg_Due,
   MIN(TotalDue) as Min_Due,
   MAX(TotalDue) as Max_Due,
-  SUM(TotalDue) as Total_Sales
+  SUM(TotalDue) as Total_Sales,
+SUM(TotalDue) / (1.0*(Avg(TotalDue))) as Number_Sales
 FROM Customer C
   JOIN Orders O ON O.CustomerID = C.CustomerID
 GROUP BY State
